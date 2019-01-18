d0f949a0233110890c521206aa499be6e94dedc6
Conrad Mearns <conradmearns@gmail.com>
2019-01-18 15:18:06 -0800

    Tweaked a few settings, prepped stufff for Git

---
+++ Assets/PongBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour
+{
+
+    public GameObject playerScore;
+    public GameObject enemyScore;
+
+    public int player_points = 0;
+    public int enemy_points = 0;
+
+    public float force = 10f;
+
+    public float speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      gameObject.GetComponent<Rigidbody>().AddForce(0, 0, -force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+      Vector3 tvel = rigidbody.velocity.normalized * speed;
+      rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, tvel, Time.deltaTime);
+
+      playerScore.GetComponent<TextMesh>().text = ""+player_points;
+      enemyScore.GetComponent<TextMesh>().text = ""+enemy_points;
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+      if(c.collider.name == "Player Wall")
+      {
+        enemy_points++;
+      }
+      else if(c.collider.name == "Enemy Wall") {
+        player_points++;
+      }
+      // Debug.Log(c.collider.name);
+    }
+
+}
---
+++ Assets/Sword.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public GameObject ball;
+    public float speed = 1.0f;
+    private float z_snap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      z_snap = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float step = speed * Time.deltaTime;
+
+      transform.position = Vector3.MoveTowards(transform.position, ball.transform.position, step);
+
+      transform.position = new Vector3(transform.position.x, transform.position.y, z_snap);
+    }
+}
